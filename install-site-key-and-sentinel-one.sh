01bd882ee2155b0a46582b838f9d5ff10d579cfa
erikberndt23 <erik@berndthaus.com>
2025-07-16 08:46:13 -0400

    deleted duplicate script

--- install-site-key-and-sentinel-one.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-# This will install SentinelOne and add the site token during the install
-echo "<SentinelOne Site Token>" > "/Library/Application Support/JAMF/Waiting Room/com.sentinelone.registration-token"
-/usr/sbin/installer -pkg "/Library/Application Support/JAMF/Waiting Room/Sentinel-Release-25-1-2-8039_macos_v25_1_2_8039.pkg" -target /
